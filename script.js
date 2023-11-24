207924922770177d5416cda23dae007a60d082dc
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-24 19:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+const arr = [1, 2, 3, 4, 5];
+async function test() {
+	output.innerText = await new Promise((res, rej)=>{
+		setTimeout(res, 1000, arr.filter(el => el % 2));
+	});
+	output.innerText = await new Promise((res, rej)=>{
+		setTimeout(res, 2000, arr.map((el) => {
+			if(el % 2 == 0) return el * 2;
+			else return el;
+		}));
+	});
+}
+test();
